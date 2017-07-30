16e8f71c7d1f8058d211ec3ce7e57b5efd94eada
localWseven <caoyu_92@126.com>
2017-07-30 17:34:46 +0800

    iphone锁屏界面

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,16 @@
-console.log("main.js is ok.");
+(function(){
+	var otime = document.getElementsByClassName("text-time")[0],
+		odate = document.getElementsByClassName("text-date")[0],
+		ochinaDate = document.getElementsByClassName("text-china-date")[0],
+		theDate = new Date(),
+
+		hour = theDate.getHours()+1,
+		min = theDate.getMinutes()+1,
+		mon = theDate.getMonth()+1,
+		date = theDate.getDate(),
+		day = theDate.getDay(),
+		cnDay = ['日','一','二','三','四','五','六'];
+
+	otime.innerText = hour + ':' + min;
+	odate.innerText = mon + '月' + date + '日 ' + '星期' + cnDay[day];
+})();
